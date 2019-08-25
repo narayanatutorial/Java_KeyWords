aceb601ccfff2bdae4ff533968b0aead127f3ae9
narayanatutorial <narayanaswamy.ragi@gmail.com>
2019-08-26 03:57:15 +0530

    initial commit

---
+++ VolatileExamples/src/main/java/com/narayanatutorial/VolatileExamples/App.java
@@ -0,0 +1,13 @@
+package com.narayanatutorial.VolatileExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
